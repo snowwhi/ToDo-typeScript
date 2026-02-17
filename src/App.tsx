b3e4f67ac38b501a36a967c510986225249584bb
snowwhi <khadijahanif890@gmail.com>
2026-02-17 01:01:57 -0500

    todo list

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Todo from './Componenet/Todo'
+
+function App() {
+
+
+  return (
+  <>
+ <div className='min-h-screen w-full bg-slate-950 flex justify-center items-center'>
+  <Todo/>
+ </div>
+  </>
+  )
+}
+
+export default App
---
+++ src/Componenet/Todo.tsx
@@ -0,0 +1,54 @@
+import React, { useState ,useRef} from 'react'
+ interface TodoItem {
+  msg: string;
+}
+const Todo = () => {
+  const ref =useRef<(HTMLHeadingElement | null)[]>([]);
+  const [msg,setMsg] =useState('')
+   const [isChecked, setIsChecking] = useState(false);
+  const [list,setList]=useState<TodoItem[]>([]);
+  const HandleSubmit =(event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      event.preventDefault()
+      console.log(msg)
+      const CopyList=[...list]
+      CopyList.push({msg})
+      setList(CopyList)
+      setMsg('')
+    }
+  }
+  const handlecomplete =(idx:number)=>{
+    const completed = !isChecked
+       setIsChecking(completed)
+       const h1ref = ref.current[idx];
+     if (completed) {
+        
+        h1ref!.style.textDecoration = 'line-through';
+        h1ref!.style.color = 'gray'; 
+      } else {
+        h1ref!.style.color='white';
+        h1ref!.style.textDecoration = 'none';
+      }
+  }
+  return (
+    <div className='w-2/3 min-h-1/2 rounded-2xl bg-white p-4'>
+     <h1 className='text-3xl font-bold text-slate-950'>Todo List</h1>
+     <input className='w-full px-4 py-2 outline-2 rounded-2xl'  type="text" placeholder='Whats the agenda of Today babe' onKeyDown={HandleSubmit}  value={msg} onChange={(e)=>setMsg(e.target.value)}/>
+     <div className='flex gap-2 flex-col' >
+
+     {list.map(function (element,idx) {
+       return(
+         <div key={idx} className='mt-2 w-full h-10 bg-slate-950 rounded-2xl px-4 py-1 flex gap-2'>
+          <input type="checkbox" name="Check" id="check"  onChange={() => handlecomplete(idx)}/>
+          <h2 className='text-2xl text-white font-bold' ref={(el) => {ref.current[idx] = el; }}>
+          {element.msg}
+          </h2>
+          </div>
+        )
+      })}
+    </div>
+      </div>
+  )
+}
+
+export default Todo
