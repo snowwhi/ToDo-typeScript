f67072ef03e39a3082cdad96aff2c8fe0e7bfbbc
snowwhi <khadijahanif890@gmail.com>
2026-02-17 13:01:56 -0500

    ui copied

--- src/Componenet/SirContext/ContextTodo.tsx
+++ src/Componenet/SirContext/ContextTodo.tsx
@@ -1,7 +1,17 @@
 
 const ContextTodo = () => {
   return (
-  <h1 className="text-9xl text-white">i am sir </h1>
+  <div className="bg-[#172842] min-h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <div className="mb-4">
+                        {/* Todo form goes here */} 
+                    </div>
+                    <div className="flex flex-wrap gap-y-3">
+                        {/*Loop and Add TodoItem here */}
+                    </div>
+                </div>
+            </div>
   )
 }
 
